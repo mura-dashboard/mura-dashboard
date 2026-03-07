b04d6234009428e69a80661f8abb75d3e4f22bd9
Andreas Keefer <andreas.keefer@proxora.com>
2026-03-07 15:28:42 +0100

    Remove Grafana and OpenTelemetry

--- mura-dashboard/src/test/java/com/github/muradashboard/app/muradashboard/TestcontainersConfiguration.java
+++ mura-dashboard/src/test/java/com/github/muradashboard/app/muradashboard/TestcontainersConfiguration.java
@@ -3,18 +3,11 @@ package com.github.muradashboard.app.muradashboard;
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
 import org.springframework.context.annotation.Bean;
-import org.testcontainers.grafana.LgtmStackContainer;
 import org.testcontainers.postgresql.PostgreSQLContainer;
 import org.testcontainers.utility.DockerImageName;
 
 @TestConfiguration(proxyBeanMethods = false)
 class TestcontainersConfiguration {
-
-    @Bean
-    @ServiceConnection
-    LgtmStackContainer grafanaLgtmContainer() {
-        return new LgtmStackContainer(DockerImageName.parse("grafana/otel-lgtm:latest"));
-    }
 
     @Bean
     @ServiceConnection
